748a38b5253ce3cfe7fdea1fb6aa0dc00c7062c2
John Trickett <johntrickett86@gmail.com>
2024-07-09 14:46:27 +0100

    update getallcustomer request
    fixed endpoint error

--- src/SimproBaseConnector.php
+++ src/SimproBaseConnector.php
@@ -5,6 +5,10 @@ namespace StitchDigital\LaravelSimproApi;
 use Exception;
 use Illuminate\Support\Facades\Cache;
 use Saloon\Http\Connector;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Saloon\PaginationPlugin\Contracts\HasPagination;
+use Saloon\PaginationPlugin\PagedPaginator;
 use Saloon\RateLimitPlugin\Contracts\RateLimitStore;
 use Saloon\RateLimitPlugin\Limit;
 use Saloon\RateLimitPlugin\Stores\LaravelCacheStore;
@@ -12,7 +16,7 @@ use Saloon\RateLimitPlugin\Traits\HasRateLimits;
 use Saloon\Traits\Plugins\AcceptsJson;
 use Saloon\Traits\Plugins\AlwaysThrowOnErrors;
 
-abstract class SimproBaseConnector extends Connector
+abstract class SimproBaseConnector extends Connector implements HasPagination
 {
     use AcceptsJson;
     use HasRateLimits;
@@ -48,17 +52,19 @@ abstract class SimproBaseConnector extends Connector
             throw new Exception('Simpro configuration error: Base URL not set');
         }
     }
+
     public function resolveBaseUrl(): string
     {
-       return $this->baseUrl . '/api/v1.0';
+        return $this->baseUrl . '/api/v1.0';
     }
 
     protected function resolveLimits(): array
     {
         return [
-            Limit::allow(config('simpro-api.rate_limit.per_second'),
-                threshold: config('simpro-api.rate_limit.threshold'))
-                ->everySeconds(seconds: 1)->sleep(),
+            Limit::allow(
+                config('simpro-api.rate_limit.per_second'),
+                threshold: config('simpro-api.rate_limit.threshold')
+            )->everySeconds(1)->sleep(),
         ];
     }
 
@@ -69,7 +75,7 @@ abstract class SimproBaseConnector extends Connector
      */
     protected function resolveRateLimitStore(): RateLimitStore
     {
-        return new LaravelCacheStore(Cache::store(config('simpro.cache.driver')));
+        return new LaravelCacheStore(Cache::store(config('simpro-api.cache.driver')));
     }
 
     /**
@@ -82,8 +88,42 @@ abstract class SimproBaseConnector extends Connector
      */
     protected function getLimiterPrefix(): ?string
     {
-        $baseUrl = $this->baseUrl ?? config('simpro-api.base_url');
+        return 'simpro-api-' . $this->baseUrl;
+    }
 
-        return 'simpro-api-' . $baseUrl;
+    /**
+     * Implement the paginate method from the HasPagination interface.
+     *
+     * @param Request $request
+     * @return PagedPaginator
+     */
+    public function paginate(Request $request): PagedPaginator
+    {
+        return new class(connector: $this, request: $request) extends PagedPaginator
+        {
+            protected function isLastPage(Response $response): bool
+            {
+                $currentPage = $this->currentPage;
+                $totalPages = (int) $response->header('Result-Pages');
+
+                return $currentPage >= $totalPages;
+            }
+
+            protected function getPageItems(Response $response, Request $request): array
+            {
+                return $response->json();
+            }
+
+            protected function applyPagination(Request $request): Request
+            {
+                $request->query()->add('page', $this->currentPage);
+
+                if (isset($this->perPageLimit)) {
+                    $request->query()->add('pageSize', $this->perPageLimit);
+                }
+
+                return $request;
+            }
+        };
     }
 }
