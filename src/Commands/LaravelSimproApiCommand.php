9889ce98d8cc01071b99ea72bc5e25d7088722ee
johntrickett86 <johntrickett86@users.noreply.github.com>
2024-10-10 14:44:31 +0000

    Fix styling

--- src/Commands/LaravelSimproApiCommand.php
+++ src/Commands/LaravelSimproApiCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Helper\Table;
 class LaravelSimproApiCommand extends Command
 {
     protected $signature = 'simpro:list-requests';
+
     protected $description = 'List all available requests in the Simpro package';
 
     public function handle(): int
@@ -19,13 +20,14 @@ class LaravelSimproApiCommand extends Command
         $requestPath = base_path('vendor/stitchdigital/laravel-simpro-api/src/Requests');
 
         // Check if the directory exists
-        if (!is_dir($requestPath)) {
+        if (! is_dir($requestPath)) {
             $this->error('Requests directory not found.');
+
             return self::FAILURE;
         }
 
         // Create a Filesystem instance to work with files
-        $filesystem = new Filesystem();
+        $filesystem = new Filesystem;
 
         // Get all PHP files in the Requests directory recursively
         $files = $filesystem->allFiles($requestPath);
@@ -36,7 +38,7 @@ class LaravelSimproApiCommand extends Command
         // Loop through the files and format the output
         foreach ($files as $file) {
             // Get the relative path from the Requests folder and format it
-            $relativePath = str_replace([$requestPath . DIRECTORY_SEPARATOR, '.php'], '', $file->getPathname());
+            $relativePath = str_replace([$requestPath.DIRECTORY_SEPARATOR, '.php'], '', $file->getPathname());
 
             // Split the path into parts
             $pathParts = explode(DIRECTORY_SEPARATOR, $relativePath);
@@ -56,6 +58,7 @@ class LaravelSimproApiCommand extends Command
         );
 
         $this->info('Request listing complete.');
+
         return self::SUCCESS;
     }
 }
