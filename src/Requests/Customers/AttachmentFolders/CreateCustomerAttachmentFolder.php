53e7b630fadeab7b08afec39c5d223b5a2375344
AElleray <a.elleray1182@gmail.com>
2024-10-12 12:51:24 +0100

    Create CreateCustomerAttachmentFolder.php

---
+++ src/Requests/Customers/AttachmentFolders/CreateCustomerAttachmentFolder.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StitchDigital\LaravelSimproApi\Requests\Customers\AttachmentFolders;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
+
+class CreateCustomerAttachmentFolder extends Request implements HasBody
+{
+    use HasJsonBody;
+    public function __construct(protected readonly int $customerId, protected readonly int $companyId, protected readonly array $data)
+    {
+        //
+    }
+
+    /**
+     * The HTTP method of the request
+     */
+    protected Method $method = Method::POST;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/companies/'.$this->companyId.'/customers/'.$this->customerId.'/attachments/folders/';
+    }
+}
