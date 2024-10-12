3d5b44858647cae0d40550008892e0f7f8323d3f
AElleray <a.elleray1182@gmail.com>
2024-10-12 19:32:06 +0100

    update

--- src/Requests/Customers/LaborRates/RetrieveCustomerLaborRate.php
+++ src/Requests/Customers/LaborRates/RetrieveCustomerLaborRate.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace StitchDigital\LaravelSimproApi\Requests\Customers\LabourRates;
+namespace StitchDigital\LaravelSimproApi\Requests\Customers\LaborRates;
 
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
--- src/Requests/Customers/LaborRates/UpdateCustomerLaborRate.php
+++ src/Requests/Customers/LaborRates/UpdateCustomerLaborRate.php
@@ -2,33 +2,33 @@
 
 declare(strict_types=1);
 
-namespace StitchDigital\LaravelSimproApi\Requests\Customers\LabourRates;
+namespace StitchDigital\LaravelSimproApi\Requests\Customers\LaborRates;
 
 use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 use Saloon\Traits\Body\HasJsonBody;
 
-class CreateCustomerLaborRate extends Request implements HasBody
+class UpdateCustomerLaborRate extends Request implements HasBody
 {
     use HasJsonBody;
 
     /**
      * @param  array<string, mixed>  $data
      */
-    public function __construct(protected readonly int $customerId, protected readonly int $companyId, protected readonly array $data)
+    public function __construct(protected readonly int $laborRateId, protected readonly int $contractId, protected readonly int $customerId, protected readonly int $companyId, protected readonly array $data)
     {
         //
     }
 
-    protected Method $method = Method::POST;
+    protected Method $method = Method::PATCH;
 
     /**
      * The endpoint for the request
      */
     public function resolveEndpoint(): string
     {
-        return '/companies/'.$this->companyId.'/customers/'.$this->customerId.'/laborRates/';
+        return '/companies/'.$this->companyId.'/customers/'.$this->customerId.'/labourRates/'.$this->laborRateId;
     }
 
     /**
