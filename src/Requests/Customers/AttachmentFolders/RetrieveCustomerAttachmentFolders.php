9d48836e7b4c54a95ffc4c62ecf774b490845a22
AElleray <a.elleray1182@gmail.com>
2024-10-12 13:03:40 +0100

    Create RetrieveCustomerAttachmentFolders.php

---
+++ src/Requests/Customers/AttachmentFolders/RetrieveCustomerAttachmentFolders.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StitchDigital\LaravelSimproApi\Requests\Customers\AttachmentFolders;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class RetrieveCustomerAttachmentFolders extends Request
+{
+    public function __construct(protected string $folderId, protected readonly int $customerId, protected int $companyId)
+    {
+        //
+    }
+
+    /**
+     * The HTTP method of the request
+     */
+    protected Method $method = Method::GET;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/companies/'.$this->companyId.'/customers/'.$this->customerId.'/attachments/folders/'.$this->folderId;
+    }
+}
