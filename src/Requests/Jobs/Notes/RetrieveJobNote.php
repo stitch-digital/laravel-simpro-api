341d827f97a325f6d902dabb252a463f5d75dec2
AElleray <a.elleray1182@gmail.com>
2024-10-21 11:10:43 +0100

    Notes

---
+++ src/Requests/Jobs/Notes/RetrieveJobNote.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StitchDigital\LaravelSimproApi\Requests\Jobs\Notes;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class RetrieveJobNote extends Request
+{
+    public function __construct(protected string $noteId, protected readonly int $quoteId, protected int $companyId)
+    {
+        //
+    }
+
+    /**
+     * The HTTP method of the request
+     */
+    protected Method $method = Method::GET;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/companies/'.$this->companyId.'/quotes/'.$this->quoteId.'/notes/'.$this->noteId;
+    }
+}
