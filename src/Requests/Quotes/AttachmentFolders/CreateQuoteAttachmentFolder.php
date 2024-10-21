e99e5fa21336a978b613b4778a3843d221c373c0
AElleray <a.elleray1182@gmail.com>
2024-10-21 10:38:31 +0100

    Update

---
+++ src/Requests/Quotes/AttachmentFolders/CreateQuoteAttachmentFolder.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StitchDigital\LaravelSimproApi\Requests\Quotes\AttachmentFolders;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
+
+class CreateQuoteAttachmentFolder extends Request implements HasBody
+{
+    use HasJsonBody;
+
+    /**
+     * @param  array<string, mixed>  $data
+     */
+    public function __construct(protected readonly int $quoteId, protected readonly int $companyId, protected readonly array $data)
+    {
+        //
+    }
+
+    protected Method $method = Method::POST;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/companies/'.$this->companyId.'/quotes/'.$this->quoteId.'/attachments/folders/';
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function defaultBody(): array
+    {
+        return $this->data;
+    }
+}
---
+++ src/Requests/Quotes/Attachments/RetrieveQuoteAttachment.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StitchDigital\LaravelSimproApi\Requests\Quotes\Attachments;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class RetrieveQuoteAttachment extends Request
+{
+    public function __construct(protected string $attachmentId, protected readonly int $quoteId, protected int $companyId)
+    {
+        //
+    }
+
+    /**
+     * The HTTP method of the request
+     */
+    protected Method $method = Method::GET;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/companies/'.$this->companyId.'/quotes/'.$this->quoteId.'/attachments/files/'.$this->attachmentId;
+    }
+}
