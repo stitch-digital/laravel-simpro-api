5895bd41322fc8a0a446670dafa28f6d924c1657
AElleray <a.elleray1182@gmail.com>
2024-10-25 12:10:44 +0100

    OneOff

---
+++ src/Requests/Quotes/Sections/CostCenters/OneOffs/UpdateQuoteSectionCostCenterOneOff.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StitchDigital\LaravelSimproApi\Requests\Quotes\Sections\CostCenters\Assets;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
+
+class UpdateQuoteSectionCostCenterOneOff extends Request implements HasBody
+{
+    use HasJsonBody;
+
+    /**
+     * @param  array<string, mixed>  $data
+     */
+    public function __construct(protected readonly int $oneOffId, protected string $costCenterId, protected string $sectionId, protected readonly int $quoteId, protected readonly int $companyId, protected readonly array $data)
+    {
+        //
+    }
+
+    protected Method $method = Method::PATCH;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/companies/'.$this->companyId.'/quotes/'.$this->quoteId.'/sections/'.$this->sectionId.'/costCenters/'.$this->costCenterId .'/oneOffs/'.$this->oneOffId;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function defaultBody(): array
+    {
+        return $this->data;
+    }
+}
