c59db608d83b77be72f06b021ec8f8f23eb60fbe
John Trickett <johntrickett86@gmail.com>
2024-12-16 14:13:35 +0000

    Update CreateQuoteSectionCostCenterAsset.php

--- src/Requests/Quotes/Sections/CostCenters/Assets/CreateQuoteSectionCostCenterAsset.php
+++ src/Requests/Quotes/Sections/CostCenters/Assets/CreateQuoteSectionCostCenterAsset.php
@@ -16,7 +16,7 @@ class CreateQuoteSectionCostCenterAsset extends Request implements HasBody
     /**
      * @param  array<string, mixed>  $data
      */
-    public function __construct(protected readonly int $sectionId, protected readonly int $quoteId, protected readonly int $companyId, protected readonly array $data)
+    public function __construct(protected readonly int $costCenterId, protected readonly int $sectionId, protected readonly int $quoteId, protected readonly int $companyId, protected readonly array $data)
     {
         //
     }
@@ -28,7 +28,7 @@ class CreateQuoteSectionCostCenterAsset extends Request implements HasBody
      */
     public function resolveEndpoint(): string
     {
-        return '/companies/'.$this->companyId.'/quotes/'.$this->quoteId.'/sections/'.$this->sectionId.'/costCenters/assets/';
+        return '/companies/'.$this->companyId.'/quotes/'.$this->quoteId.'/sections/'.$this->sectionId.'/costCenters/'.$this->costCenterId.'/assets/';
     }
 
     /**
